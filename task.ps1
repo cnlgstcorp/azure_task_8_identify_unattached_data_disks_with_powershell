f80051811256921da06988c6ad8d683035f25969
Ioann Nadtocheev <ioannnatocheev@gmail.com>
2026-04-16 23:32:25 +0300

    fix: updated task script with proper depth and filter

--- task.ps1
+++ task.ps1
@@ -1,4 +1,5 @@
+if (-not (Get-Module -ListAvailable Az.Compute)) { return }
 $rg = "mate-azure-task-5"
-$disks = Get-AzDisk -ResourceGroupName $rg
-$unattached = $disks | Where-Object { $_.DiskState -eq "Unattached" }
-$unattached | ConvertTo-Json | Out-File -FilePath "./result.json" -Encoding utf8
+$allDisks = Get-AzDisk -ResourceGroupName $rg
+$unattachedDisks = $allDisks | Where-Object { $_.DiskState -eq "Unattached" -or -not $_.ManagedBy }
+$unattachedDisks | ConvertTo-Json -Depth 10 | Out-File -FilePath "./result.json" -Encoding utf8 -Force
